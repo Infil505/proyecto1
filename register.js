a3543c3cdf204709146fbb032c21dba2654b9140
Darwin Javier Silva Gadea <dsilvagadea@gmail.com>
2023-10-03 14:40:45 -0600

    mejoras

--- register.js
+++ register.js
@@ -3,7 +3,7 @@ const usersDB = JSON.parse(localStorage.getItem("users")) || [];
 document.addEventListener("DOMContentLoaded", function () {
     const registrationForm = document.getElementById("registration-form");
     const userTypeSelect = document.getElementById("user-type");
-    const admin123Hex = "61646D696E313233"; 
+    const passHex = "61646D696E313233"; 
 
     function hexToString(hex) {
         return hex.match(/.{1,2}/g).map(byte => String.fromCharCode(parseInt(byte, 16))).join('');
@@ -21,20 +21,29 @@ document.addEventListener("DOMContentLoaded", function () {
     registrationForm.addEventListener("submit", function (e) {
         e.preventDefault();
         const username = document.getElementById("reg-username").value;
-        const password = document.getElementById("reg-password").value;
+        const passwordField = document.getElementById("reg-password");
+        const password = passwordField.value;
         const email = document.getElementById("reg-email").value;
         const userType = userTypeSelect.value;
         const entered = userType === "empleado" ? prompt("Por favor, verifique su identidad de administrador con su clave de acceso al sistema") : "";
 
         if (usersDB.some(user => user.username === username)) {
             showAlert("El usuario ya existe. Por favor, elija otro nombre de usuario.");
-        } else if (userType === "empleado" && entered !== hexToString(admin123Hex)) {
+        } else if (userType === "empleado" && entered !== hexToString(passHex)) {
             showAlert('Contraseña de administrador incorrecta. El registro ha fallado.');
         } else {
-            addUserToDB(username, userType === "empleado" ? hexToString(admin123Hex) : password, email, userType);
+            addUserToDB(username, password, email, userType);
             showAlert(`Registro exitoso. Ahora puede iniciar sesión como ${userType === "empleado" ? "empleado" : "usuario"}.`);
             registrationForm.reset();
             window.location.href = "login.html";
         }
     });
+
+    // Cambiar el tipo de entrada a contraseña al enfocar el campo de contraseña
+    const passwordField = document.getElementById("reg-password");
+    passwordField.addEventListener("focus", function () {
+        passwordField.type = "password";
+    });
 });
+
+
